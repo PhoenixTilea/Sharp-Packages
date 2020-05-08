4276314579c676bdb1ffedcfc1f8aa053dc8baf4
Shaarigan <Shaarigan@users.noreply.github.com>
2020-05-09 00:53:09 +0200

    Version 1.1.0

---
+++ SE@Npm/Sharp/Package/PackageRepository.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (C) 2017 Schroedinger Entertainment
+// Distributed under the Schroedinger Entertainment EULA (See EULA.md for details)
+
+using System;
+using System.Collections.Generic;
+
+namespace SE.Npm
+{
+    /// <summary>
+    /// Provides information about a repository inside a package info
+    /// </summary>
+    public struct PackageRepository
+    {
+        string type;
+        /// <summary>
+        /// The protocol type used to access the repository
+        /// </summary>
+        public string Type
+        {
+            get { return type; }
+        }
+
+        Uri url;
+        /// <summary>
+        /// The address of the repository
+        /// </summary>
+        public Uri Url
+        {
+            get { return url; }
+        }
+
+        string directory;
+        /// <summary>
+        /// An optional path offset to the package.json file
+        /// </summary>
+        public string Directory
+        {
+            get { return directory; }
+        }
+
+        /// <summary>
+        /// Creates a new repository info instance
+        /// </summary>
+        public PackageRepository(string type, Uri url, string directory)
+        {
+            this.type = type;
+            this.url = url;
+            this.directory = directory;
+        }
+    }
+}
